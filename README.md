e71aa3d1c7c87e2f7bbf8b5fe676413efe88d0fb
Shreyas Rao <raoshreyas2004@gmail.com>
2024-06-26 22:52:28 -0400

    Basic setup of frontend web app and tailwind

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Stock-Portfolio
+
+This is a full stack application with elements of data mining and analysis using a database engine.
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,8 +5,11 @@ import "dotenv/config";
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// application/json parser
 app.use(bodyParser.json());
+
+// application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
   console.log("HTTP server on http://localhost:%s", PORT);
