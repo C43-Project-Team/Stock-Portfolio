e3c5e14b93fc8943399d30453ea44b522ee8683e
Murphy Lee <murphylee2004@gmail.com>
2024-07-06 16:46:39 -0400

    temp: skeleton for creating table

---
+++ backend/src/migrations/TableManager.ts
@@ -0,0 +1,13 @@
+import { db } from "../utils/database";
+
+class TableManager {
+  async createTables() {
+    // TODO
+  }
+}
+
+const tableManager = new TableManager();
+tableManager
+  .createTables()
+  .then(() => console.log("Tables created"))
+  .catch(console.error);
