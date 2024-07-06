f4b8021a6ba841bc3f94e0417e46b3d7b1731ea5
Murphy Lee <murphylee2004@gmail.com>
2024-07-06 16:27:59 -0400

    fix: some package stuff

--- backend/src/utils/database.ts
+++ backend/src/utils/database.ts
@@ -1,6 +1,6 @@
-import { Database } from '../types/db-schema';
-import { Pool } from 'pg';
-import { Kysely, PostgresDialect } from 'kysely';
+import { Database } from "../types/db-schema";
+import { Pool } from "pg";
+import { Kysely, PostgresDialect } from "kysely";
 import "dotenv/config";
 
 const dialect = new PostgresDialect({
@@ -8,12 +8,12 @@ const dialect = new PostgresDialect({
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
-    port: process.env.DATABASE_PORT,
+    port: parseInt(process.env.DATABASE_PORT || "", 10),
     password: process.env.DATABASE_PASSWORD,
     max: 10,
-  })
-})
+  }),
+});
 
 export const db = new Kysely<Database>({
   dialect,
-})
+});
