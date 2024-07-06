2b98547bfdfa06d299f4a42c71ad199b9b66975a
Murphy Lee <murphylee2004@gmail.com>
2024-07-06 16:04:29 -0400

    feat: wrap up db schema

--- backend/src/types/db-schema.ts
+++ backend/src/types/db-schema.ts
@@ -1,17 +1,17 @@
 import {
-    ColumnType,
-    Generated,
-    Insertable,
-    Selectable,
-    Updateable,
+  ColumnType,
+  Generated,
+  Insertable,
+  Selectable,
+  Updateable,
 } from "kysely";
 
 export interface UsersTable {
-    username: string;
-    password_hash: string;
-    first_name: string;
-    surname: string;
-    user_created_at: ColumnType<Date, string | undefined, never>;
+  username: string;
+  password_hash: string;
+  first_name: string;
+  surname: string;
+  user_created_at: ColumnType<Date, string | undefined, never>;
 }
 
 export type User = Selectable<UsersTable>;
@@ -19,10 +19,10 @@ export type NewUser = Insertable<UsersTable>;
 export type UserUpdate = Updateable<UsersTable>;
 
 export interface PortfoliosTable {
-    id: Generated<number>;
-    portfolio_name: string;
-    cash: number;
-    portfolio_created_at: ColumnType<Date, string | undefined, never>;
+  id: Generated<number>;
+  portfolio_name: string;
+  cash: number;
+  portfolio_created_at: ColumnType<Date, string | undefined, never>;
 }
 
 export type Portfolio = Selectable<PortfoliosTable>;
@@ -40,8 +40,8 @@ export type NewStocksList = Insertable<StocksListTable>;
 export type StocksListUpdate = Updateable<StocksListTable>;
 
 export interface StocksTable {
-    stock_symbol: string;
-    company: string;
+  stock_symbol: string;
+  company: string;
 }
 
 export type Stocks = Selectable<StocksTable>;
@@ -63,44 +63,89 @@ export type NewStocksDaily = Insertable<StocksDailyTable>;
 export type StocksDailyUpdate = Updateable<StocksDailyTable>;
 
 export interface ReviewsTable {
-    user_id: number;
-    stock_list_id: number;
-    content: string;
-    review_creation_time: ColumnType<Date, string | undefined, never>;
-    review_last_update: ColumnType<Date, string | undefined, never>;
+  user_id: number;
+  stock_list_id: number;
+  content: string;
+  review_creation_time: ColumnType<Date, string | undefined, never>;
+  review_last_updated: ColumnType<Date, string | undefined, never>;
 }
 
-export type Reviews = Selectable<ReviewsTable>;
-export type NewReviews = Insertable<ReviewsTable>;
-export type ReviewsUpdate = Updateable<ReviewsTable>;
+export type Review = Selectable<ReviewsTable>;
+export type NewReview = Insertable<ReviewsTable>;
+export type ReviewUpdate = Updateable<ReviewsTable>;
+
+export interface FriendsTable {
+  friend1: number;
+  friend2: number;
+  requesting_friend: number;
+  pending: boolean;
+}
+
+export type Friend = Selectable<FriendsTable>;
+export type NewFriend = Insertable<FriendsTable>;
+export type FriendUpdate = Updateable<FriendsTable>;
 
 export interface RequestTimeoutTable {
-    request_user: number;
-    receive_user: number;
-    expiry_time: ColumnType<Date, string | undefined, never>;
+  request_user: number;
+  receive_user: number;
+  expiry_time: ColumnType<Date, string | undefined, never>;
 }
 
 export type RequestTimeout = Selectable<RequestTimeoutTable>;
 export type NewRequestTimeout = Insertable<RequestTimeout>;
 export type RequestTimeoutUpdate = Updateable<RequestTimeout>;
 
+export interface OwnsTable {
+  portfolio_id: number;
+  user_id: number;
+}
+
+export type Owns = Selectable<OwnsTable>;
+export type NewOwns = Insertable<OwnsTable>;
+export type OwnsUpdate = Updateable<OwnsTable>;
+
 export interface InvestmentsTable {
-    portfolio_id: number;
-    stock_symbol: string;
-    stock_date: ColumnType<Date, string | undefined, never>;
-    num_shares: number;
+  portfolio_id: number;
+  stock_symbol: string;
+  stock_date: ColumnType<Date, string | undefined, never>;
+  num_shares: number;
 }
 
 export type Investments = Selectable<InvestmentsTable>;
 export type NewInvestments = Insertable<InvestmentsTable>;
 export type InvestmentsTableUpdate = Updateable<InvestmentsTable>;
 
+export interface AccessTable {
+  user_id: number;
+  stock_list_id: number;
+  is_owner: boolean;
+}
+
+export type Access = Selectable<AccessTable>;
+export type NewAccess = Insertable<AccessTable>;
+export type AccessUpdate = Updateable<AccessTable>;
+
 export interface ContainsTable {
-    stock_list_id: number;
-    stock_symbol: string;
-    num_shares: number;
+  stock_list_id: number;
+  stock_symbol: string;
+  num_shares: number;
 }
 
 export type Contains = Selectable<ContainsTable>;
 export type NewContains = Insertable<ContainsTable>;
 export type ContainsUpdate = Updateable<ContainsTable>;
+
+export interface Database {
+  users: UsersTable;
+  portfolios: PortfoliosTable;
+  stocks_list: StocksListTable;
+  stocks: StocksTable;
+  stocks_daily: StocksDailyTable;
+  reviews: ReviewsTable;
+  friends: FriendsTable;
+  request_timeout: RequestTimeoutTable;
+  owns: OwnsTable;
+  investments: InvestmentsTable;
+  access: AccessTable;
+  contains: ContainsTable;
+}
