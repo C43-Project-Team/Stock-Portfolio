eeb1556580cc45bcec35b87464b23810a1e83756
murphylee10 <murphylee2004@gmail.com>
2024-08-01 12:56:29 -0400

    fix: routing

--- web/src/app/pages/landing/landing.component.ts
+++ web/src/app/pages/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
-import type { Observable } from 'rxjs';
+import { catchError, map, of, type Observable } from 'rxjs';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 // biome-ignore lint/style/useImportType: angular needs the whole module for elements passed in constructor 
@@ -21,11 +21,15 @@ import { AuthService } from '@services/auth.service';
 })
 export class LandingComponent implements OnInit {
   isAuthenticated$!: Observable<boolean>;
+  user$!: Observable<any>;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.isAuthenticated$ = this.authService.isAuthenticated();
+    this.user$ = this.authService.getCredentials().pipe(
+      catchError(() => of(null))
+    );
   }
 
   navigateTo(path: string) {
