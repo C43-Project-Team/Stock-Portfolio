f17f6984f86ec54b6a4604589df45fcb2e5669a6
murphylee10 <murphylee2004@gmail.com>
2024-08-01 13:03:38 -0400

    fix: delete useless home page

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import type { Routes } from "@angular/router";
 import { SignInComponent } from "./pages/sign-in/sign-in.component";
 import { SignUpComponent } from "./pages/sign-up/sign-up.component";
-import { HomeComponent } from "./pages/home/home.component";
 import { LayoutComponent } from "@pages/layout/layout.component";
 import { Component } from "@angular/core";
 import { ConnectionsComponent } from "@pages/connections/connections.component";
